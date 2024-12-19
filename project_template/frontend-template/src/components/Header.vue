264c8db61a0ec03062928620fdf8e5546203374f
nunu1101 <soobinnunu1101@gmail.com>
2024-12-19 17:47:07 +0900

    Style 변경

--- project_template/frontend-template/src/components/Header.vue
+++ project_template/frontend-template/src/components/Header.vue
@@ -3,7 +3,9 @@
     <div class="commonHeaderContent">
       <h2><a href="#" class="logo"><img alt="Vue logo" src="../assets/logo.svg" width="20"></a> {{ message }}</h2>
       <ul class="menu">
-        <li><router-link to="/josaTargetList">조사업무</router-link> | </li>
+        <li><router-link to="/josaTargetList">조사자</router-link>  </li>
+        <li><router-link to="/josaTargetList">조사대상</router-link>  </li>
+        <li><router-link to="/josaTargetList">조사업무</router-link> </li>
       </ul>
     </div>
   </div>
@@ -17,36 +19,32 @@ const message = ref('조사데이터 관리시스템');
 
 <style scoped>
 .commonHeader {
-  position: fixed; /* Change from relative to fixed */
-  top: 0; /* Align to the top of the page */
-  left: 0; /* Ensure it spans the full width */
-  width: 100%; /* Full width */
-  height: 50px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
   box-shadow: 0 0px 30px rgba(0, 0, 0, 0.1);
   line-height: 48px;
   padding: 0 10px;
   background-color: #ffffff;
-  z-index: 1000; /* High z-index to ensure it stays on top */
+  z-index: 1000;
 }
 
 .commonHeaderContent {
   width: 100%;
   max-width: 1280px;
-  margin: 0 auto;
   line-height: 50px;
   position: relative;
+  display: flex;
+  align-items: center;
 }
 
-h1 {
-  position: absolute;
-  top: 0;
-  left: 0; /* Adjusted to align with the left side */
+h2 {
+  margin: 0;
+  padding: 0;
 }
 
 ul.menu {
-  position: absolute;
-  top: 0; /* Aligned with the top */
-  right: 0; /* Aligned with the right side */
   margin: 0;
   padding: 0;
 }
